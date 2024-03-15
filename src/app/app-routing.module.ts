37627c241d72b37d4cfc23339c8409faac13a400
A Venkatesh <avenkateshanand1998@gmail.com>
2024-03-15 12:47:57 +0530

    i have to do the routing. navigate one component to the other component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,53 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './Pages/home/home.component';
+import { AboutComponent } from './Pages/about/about.component';
+import { ProductsComponent } from './Pages/products/products.component';
+import { AdminComponent } from './Pages/admin/admin.component';
+import { ContactComponent } from './Pages/contact/contact.component';
+import { RegisterComponent } from './Pages/register/register.component';
+import { LoginComponent } from './Pages/login/login.component';
+import { BlogComponent } from './Pages/blog/blog.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:"",
+    redirectTo:"/Home",
+    pathMatch:"full"
+  },
+  {
+    path:"Home",
+    component:HomeComponent
+  },
+  {
+    path:"About",
+    component:AboutComponent
+  },
+  {
+    path:"Products",
+    component:ProductsComponent
+  },
+  {
+    path:"Admin",
+    component:AdminComponent
+  },
+  {
+    path:"Contact",
+    component:ContactComponent
+  },
+  {
+    path:"Register",
+    component:RegisterComponent
+  },
+  {
+    path:"Login",
+    component:LoginComponent
+  },
+  {
+    path:"Blog",
+    component:BlogComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
