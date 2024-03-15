425bb2e0b78ba1163ebe8f9583fb1f9e6dea94e8
A Venkatesh <avenkateshanand1998@gmail.com>
2024-03-15 12:37:12 +0530

    Multiple Components Creations #8

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './Pages/home/home.component';
+import { AboutComponent } from './Pages/about/about.component';
+import { ProductsComponent } from './Pages/products/products.component';
+import { RegisterComponent } from './Pages/register/register.component';
+import { LoginComponent } from './Pages/login/login.component';
+import { ContactComponent } from './Pages/contact/contact.component';
+import { AdminComponent } from './Pages/admin/admin.component';
+import { BlogComponent } from './Pages/blog/blog.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    ProductsComponent,
+    RegisterComponent,
+    LoginComponent,
+    ContactComponent,
+    AdminComponent,
+    BlogComponent
   ],
   imports: [
     BrowserModule,
